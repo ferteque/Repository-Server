62853814692552f68a4d7bb9cda9effdc905e7a4
ferteque <ferteque98@gmail.com>
2025-04-19 17:24:46 +0200

    Adding new origin

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/process": {"origins": "https://repository-server.onrender.com"}})
+CORS(app, resources={r"/process": {"origins": ["https://repository-server.onrender.com" , "https://striking-orella-ferteque-e35fe763.koyeb.app"]}})
 
 @app.route('/')
 def home():
