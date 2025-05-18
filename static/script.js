4e75d2769d0083bdf7034b26745f02ce04a50262
ferteque <ferteque98@gmail.com>
2025-05-18 16:45:58 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -20,6 +20,9 @@
                     event.preventDefault();
                     const form = event.target;
                     const formData = new FormData(form);
+                    for (let [key, value] of formData.entries()) {
+                        console.log(`${key}:`, value);
+                    }
                     submitPlaylist(formData);  
                 });
            
