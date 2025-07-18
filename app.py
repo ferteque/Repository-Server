a24c552ac9cefe3dabf636be4aa53aca861ddb58
ferteque <ferteque98@gmail.com>
2025-07-19 00:59:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -320,6 +320,10 @@ def update_playlist():
             try:
                 logging.info(f"Group name: {group_name}")
                 cursor.execute(
+                    "DELETE FROM categories WHERE list_id = %s", (playlist_id),
+                    (playlist_id, group_name, 0)
+                )
+                cursor.execute(
                     "INSERT INTO categories (list_id, name, auto_update) VALUES (%s, %s, %s)",
                     (playlist_id, group_name, 0)
                 )
